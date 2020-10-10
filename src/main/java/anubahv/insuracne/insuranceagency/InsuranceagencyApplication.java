6f86c903d2798877930e7e32a96411797dd9295d
Kavyansh Gangwar <kavyanshgangwar.cse18@itbhu.ac.in>
2020-10-10 16:00:48 +0530

    made policy repository. closes #21

--- src/main/java/anubahv/insuracne/insuranceagency/InsuranceagencyApplication.java
+++ src/main/java/anubahv/insuracne/insuranceagency/InsuranceagencyApplication.java
@@ -1,11 +1,9 @@
 package anubahv.insuracne.insuranceagency;
 
-import anubahv.insuracne.insuranceagency.models.User;
-import anubahv.insuracne.insuranceagency.services.UserService;
-import org.springframework.boot.ApplicationRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class InsuranceagencyApplication {
@@ -13,5 +11,6 @@ public class InsuranceagencyApplication {
     public static void main(String[] args) {
         SpringApplication.run(InsuranceagencyApplication.class, args);
     }
+
 
 }
