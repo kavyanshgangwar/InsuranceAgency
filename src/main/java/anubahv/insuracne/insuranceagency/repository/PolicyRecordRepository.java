7665fd292ac7459e789a8124f17dac81b32d3e14
Kavyansh Gangwar <kavyanshgangwar.cse18@itbhu.ac.in>
2020-10-11 15:17:50 +0530

    created the policy record repository. closes #32

---
+++ src/main/java/anubahv/insuracne/insuranceagency/repository/PolicyRecordRepository.java
@@ -0,0 +1,16 @@
+package anubahv.insuracne.insuranceagency.repository;
+
+import anubahv.insuracne.insuranceagency.models.PolicyRecord;
+
+import java.util.Date;
+import java.util.List;
+
+public interface PolicyRecordRepository {
+    public void save(PolicyRecord policyRecord);
+    public PolicyRecord getPolicyRecord(int id);
+    public void delete(int id);
+    public void changeExpiryDate(Date date,int id);
+    public List<PolicyRecord> findAllOfUser(int userId);
+    public List<PolicyRecord> findAllOfUserOfCategory(int userId,String category);
+    public void changeStatus(String status,int id);
+}
