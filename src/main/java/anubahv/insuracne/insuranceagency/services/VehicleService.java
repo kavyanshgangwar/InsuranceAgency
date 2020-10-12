d1f8b493c1adf9fd0c1ba74dd591a39461891ceb
Kavyansh Gangwar <kavyanshgangwar.cse18@itbhu.ac.in>
2020-10-12 19:18:51 +0530

    created vehicle service. closes #41

---
+++ src/main/java/anubahv/insuracne/insuranceagency/services/VehicleService.java
@@ -0,0 +1,15 @@
+package anubahv.insuracne.insuranceagency.services;
+
+import anubahv.insuracne.insuranceagency.models.PolicyRecord;
+import anubahv.insuracne.insuranceagency.models.Vehicle;
+
+import java.util.List;
+
+public interface VehicleService {
+    public List<Vehicle> getByUser(int userId);
+    public Vehicle getById(int id);
+    public PolicyRecord getRecordOnVehicle(int id);
+    public void addVehicle(Vehicle vehicle);
+    public void changeRecord(int recordId,int id);
+    public void delete(int id);
+}
---
+++ src/main/java/anubahv/insuracne/insuranceagency/services/VehicleServiceImpl.java
@@ -0,0 +1,52 @@
+package anubahv.insuracne.insuranceagency.services;
+
+import anubahv.insuracne.insuranceagency.models.PolicyRecord;
+import anubahv.insuracne.insuranceagency.models.Vehicle;
+import anubahv.insuracne.insuranceagency.repository.PolicyRecordRepository;
+import anubahv.insuracne.insuranceagency.repository.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VehicleServiceImpl implements VehicleService {
+    VehicleRepository vehicleRepository;
+    PolicyRecordRepository policyRecordRepository;
+
+    @Autowired
+    public VehicleServiceImpl(VehicleRepository vehicleRepository, PolicyRecordRepository policyRecordRepository) {
+        this.vehicleRepository = vehicleRepository;
+        this.policyRecordRepository = policyRecordRepository;
+    }
+
+    @Override
+    public List<Vehicle> getByUser(int userId) {
+        return vehicleRepository.findByUser(userId);
+    }
+
+    @Override
+    public Vehicle getById(int id) {
+        return vehicleRepository.findById(id);
+    }
+
+    @Override
+    public PolicyRecord getRecordOnVehicle(int id) {
+        return policyRecordRepository.getPolicyRecord(vehicleRepository.findRecordOnVehicle(id));
+    }
+
+    @Override
+    public void addVehicle(Vehicle vehicle) {
+        vehicleRepository.save(vehicle);
+    }
+
+    @Override
+    public void changeRecord(int recordId, int id) {
+        vehicleRepository.changeRecord(recordId,id);
+    }
+
+    @Override
+    public void delete(int id) {
+        vehicleRepository.delete(id);
+    }
+}
