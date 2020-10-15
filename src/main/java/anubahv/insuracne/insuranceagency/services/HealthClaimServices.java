e1a28d94ad5ac24fe2333e7670ef5ddbd8d3df51
Kavyansh Gangwar <kavyanshgangwar.cse18@itbhu.ac.in>
2020-10-15 20:56:26 +0530

    created the health claims service. closes #53

---
+++ src/main/java/anubahv/insuracne/insuranceagency/services/HealthClaimServices.java
@@ -0,0 +1,13 @@
+package anubahv.insuracne.insuranceagency.services;
+
+import anubahv.insuracne.insuranceagency.models.HealthClaim;
+
+import java.util.List;
+
+public interface HealthClaimServices {
+    public HealthClaim getClaim(int id);
+    public void add(HealthClaim vehicleClaims);
+    public void changeStatus(String status,int id);
+    public List<String> getRelatedDocuments(int id);
+    public void delete(int id);
+}
