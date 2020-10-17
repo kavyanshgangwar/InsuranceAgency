55dc2d08e96f6b85b285d2251aa0676afc329b6f
Kavyansh Gangwar <kavyanshgangwar.cse18@itbhu.ac.in>
2020-10-17 15:13:34 +0530

    made the property page. closes #46

--- src/main/java/anubahv/insuracne/insuranceagency/controllers/UserProfileController.java
+++ src/main/java/anubahv/insuracne/insuranceagency/controllers/UserProfileController.java
@@ -1,23 +1,39 @@
 package anubahv.insuracne.insuranceagency.controllers;
 
+import anubahv.insuracne.insuranceagency.models.Property;
 import anubahv.insuracne.insuranceagency.models.User;
-import anubahv.insuracne.insuranceagency.services.SecurityService;
-import anubahv.insuracne.insuranceagency.services.UserService;
+import anubahv.insuracne.insuranceagency.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class UserProfileController {
     UserService userService;
     SecurityService securityService;
+    PropertyService propertyService;
+    VehicleService vehicleService;
+    PolicyRecordService policyRecordService;
+    PropertyClaimsServices propertyClaimsServices;
+    VehicleClaimsService vehicleClaimsService;
+    HealthClaimServices healthClaimServices;
+    LifeInsuranceClaimService lifeInsuranceClaimService;
 
     @Autowired
-    public UserProfileController(UserService userService,SecurityService securityService) {
+    public UserProfileController(UserService userService, SecurityService securityService, PropertyService propertyService, VehicleService vehicleService, PolicyRecordService policyRecordService, PropertyClaimsServices propertyClaimsServices, VehicleClaimsService vehicleClaimsService, HealthClaimServices healthClaimServices, LifeInsuranceClaimService lifeInsuranceClaimService) {
         this.userService = userService;
-        this.securityService =securityService;
+        this.securityService = securityService;
+        this.propertyService = propertyService;
+        this.vehicleService = vehicleService;
+        this.policyRecordService = policyRecordService;
+        this.propertyClaimsServices = propertyClaimsServices;
+        this.vehicleClaimsService = vehicleClaimsService;
+        this.healthClaimServices = healthClaimServices;
+        this.lifeInsuranceClaimService = lifeInsuranceClaimService;
     }
 
     @RequestMapping("/self")
@@ -29,5 +45,17 @@ public class UserProfileController {
         User user =  userService.findByUsername(loggedInUserName);
         model.addAttribute("user",user);
         return "profile/userprofile";
+    }
+
+    @RequestMapping("/self/property")
+    public String propertyPage(Model model){
+        String loggedInUserName = securityService.findLoggedInUsername();
+        if(loggedInUserName==null){
+            return "redirect:/login";
+        }
+        User user = userService.findByUsername(loggedInUserName);
+        List<Property> properties = propertyService.getByUser(user.getId());
+        model.addAttribute("properties",properties);
+        return "profile/property";
     }
 }
