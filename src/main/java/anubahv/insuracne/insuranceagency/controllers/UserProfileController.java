3c6e72cd6c3ae8712ad1c9a462884a4f4b2d5316
Kavyansh Gangwar <kavyanshgangwar.cse18@itbhu.ac.in>
2020-10-17 15:00:45 +0530

    made the profile page. closes #78

--- src/main/java/anubahv/insuracne/insuranceagency/controllers/UserProfileController.java
+++ src/main/java/anubahv/insuracne/insuranceagency/controllers/UserProfileController.java
@@ -1,12 +1,12 @@
 package anubahv.insuracne.insuranceagency.controllers;
 
+import anubahv.insuracne.insuranceagency.models.User;
 import anubahv.insuracne.insuranceagency.services.SecurityService;
 import anubahv.insuracne.insuranceagency.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,8 +25,9 @@ public class UserProfileController {
         String loggedInUserName = securityService.findLoggedInUsername();
         if(loggedInUserName==null){
             return "redirect:/login";
-        }else{
-            return "profile/userprofile";
         }
+        User user =  userService.findByUsername(loggedInUserName);
+        model.addAttribute("user",user);
+        return "profile/userprofile";
     }
 }
